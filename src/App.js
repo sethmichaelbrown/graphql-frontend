ff9cb7ebd9fc98ead0ad8cb22359dc07889d6da1
Seth Brown <seth.brown@ihsmarkit.com>
2020-02-20 15:02:41 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div classNameName="App">
+        <NavBar />
+      </div>
+    );
+  }
 }
 
 export default App;
