be69c5d274d2ffb6567f16fd05779d1d42f56a84
Seth Brown <seth.brown@ihsmarkit.com>
2020-03-04 12:58:04 -0700

    Mutation by id works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
 import React, { PureComponent } from "react";
-import NavBar from "./components/NavBar";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import "./App.css";
 
+// const userQuery = gql`
+//   {
+//     users {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `;
+
+const userByIdQuery = gql`
+  query usersById($id: Int!) {
+    usersById(id: $id) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+const NameList = ({ id }) => {
+  console.log(id);
+  return (
+    id && (
+      <Query query={userByIdQuery} variables={{ id: Number(id) }}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const users = [data.usersById];
+
+          return (
+            <div>
+              {users.map(i => (
+                <div className="col-12" key={i.id}>
+                  {i.firstName} {i.lastName}
+                </div>
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    )
+  );
+};
+
 class App extends PureComponent {
+  state = {
+    id: null
+  };
+
+  fetchUserById = async () => {
+    const res = await this.props.client
+      .query({
+        query: userByIdQuery,
+        variables: {
+          id: 1
+        }
+      })
+      .catch(e => console.warn(e));
+    console.log(res);
+  };
+
   render() {
     return (
-      <div classNameName="App">
-        <NavBar />
+      <div className="App container">
+        <label className="">Thing</label>
+        <input
+          type="text"
+          onChange={e =>
+            this.setState({
+              id: e.target.value
+            })
+          }
+        />
+        <NameList id={this.state.id} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <App client={client} />
   </ApolloProvider>,
   document.getElementById("root")
 );
